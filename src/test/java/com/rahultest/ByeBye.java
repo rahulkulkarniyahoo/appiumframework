7c657c99a367dbb9f1ca529e7192e049d6c2928a
Rahul Kulkarni <rahul.kulkarni@yahoo.com>
2017-09-22 15:53:47 +0100

    Bye Bye Class

---
+++ src/test/java/com/rahultest/ByeBye.java
@@ -0,0 +1,11 @@
+package com.rahultest;
+
+/**
+ * Created by rahulk on 22/09/2017.
+ */
+public class ByeBye {
+    public static void main(String [] args){
+
+        System.out.println("Bye bye");
+    }
+}
