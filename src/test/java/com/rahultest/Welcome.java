89b44ba5b07515638981ed3572c230d3e7457b76
Rahul Kulkarni <rahul.kulkarni@yahoo.com>
2017-09-22 15:52:32 +0100

    First Class - Welcome

---
+++ src/test/java/com/rahultest/Welcome.java
@@ -0,0 +1,11 @@
+package com.rahultest;
+
+/**
+ * Created by rahulk on 22/09/2017.
+ */
+public class Welcome {
+    public static void main(String[] args){
+
+        System.out.println("Welcome!");
+    }
+}
